625ef9320914dc0c15d70c58b013c101e48ead65
0KOMRA <88561183+0KOMRA@users.noreply.github.com>
2021-08-07 11:19:46 +0300

    Add files via upload

---
+++ firebase auth/index.html/auth.js
@@ -0,0 +1,10 @@
+//login form
+const loginForm = document.querySelector('#login_form');
+console.log(loginForm);
+poppy=document.getElementById('do_login')
+poppy({
+  const email = document.getElementById('#user')
+  const password =document.getElementById('#pass')
+
+  console.log(email, password)
+})
